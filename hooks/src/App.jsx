82df94a5755916bd9c7a93ce5b613118f860b9c1
Gisele ROncoleta <gisele.roncoleta@fatec.sp.gov.br>
2025-04-04 09:04:01 -0300

    (04/04) Exercicio 02

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 import Exercicio01 from './exercicios/01'
+import Exercicio02 from './exercicios/02'
+import Exercicio03 from './exercicios/03'
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <ul>
           <li> <Link to="/">Início</Link> </li>
           <li> <Link to="/01">Exercício 01</Link> </li>
+          <li> <Link to="/02">Exercício 02</Link> </li>
+          <li> <Link to="/03">Exercício 03</Link> </li>
         </ul>
         
         <hr />
@@ -22,6 +26,8 @@ function App() {
           } />
 
           <Route path="/01" element={ <Exercicio01 /> } />
+          <Route path="/02" element={ <Exercicio02 /> } />
+          <Route path="/03" element={ <Exercicio03 /> } />
         </Routes>
       </BrowserRouter>
     </>
