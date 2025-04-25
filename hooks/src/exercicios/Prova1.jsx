939c17b6365ce060e14b58d0a073daec4aa1e6c3
Gisele ROncoleta <gisele.roncoleta@fatec.sp.gov.br>
2025-04-25 10:53:30 -0300

    (Prova1)

--- hooks/src/exercicios/Prova1.jsx
+++ hooks/src/exercicios/Prova1.jsx
@@ -1,57 +1,71 @@
 import React, { useState } from 'react'
 
 function Prova1() {
-	const [preco,setPreco] = useState(0);
-	const [desconto,setDesconto] = useState(0); 
+	const [preco, setPreco] = useState(0);
+	const [desconto, setDesconto] = useState(0);
 
-	const handleChange(event) =>{
-		const {preco, desconto} = event.target;
+	const handleChange = (event) => {
+		const { name, value } = event.target;
 
 		if (name === 'preco') {
 			setPreco(Number(value));
-		} 
-		else if (name --- 'desconto')  {
-			setDesconto(Number(value));
-					
 		}
-	};	
+		else if (name-- - 'desconto') {
+			setDesconto(Number(value));
+
+		}
+	};
 
 	//Calcular o preco com desconto
 	const calcularPrecoComDesconto = () => {
-		return calcularPrecoComDesconto = () => {
-			return preço - (preco * (desconto/100));
-		};
-			
+		return preco - (preco * (desconto / 100));
+	};
+
+}
+
+//VAriavel para guardar o valor com desconto
+const precoDesconto = calculaPrecoDesconto();
+
+//Funcao que retorna a classe com desconto
+const classDesconto = () => {
+	if (desconto <= 15) {
+		return "Desconto Negocial";
+	} else if (desconto > 15 && desconto <= 50) {
+		return "Desconto Promocional";
+	} else if (desconto > 50) {
+		return "Desconto Liquidação";
+	} else {
+		return "Sem classificação";
 	}
-	return (<main>
+};
+
+return (
+	<main>
 		<h1>Cálculo de descontos</h1>
 
 		<div id="container">
 			<div className="input-set">
 				<label>
 					<span>Preço cheio</span><br />
-					<input name="preco" type="number" value={preco} onChange={ (e) =>
-						setPreco(Number (e.target.value))
-					} />
+					<input name="preco" type="number" value={preco} onChange={handleChange}
+					/>
 				</label>
 			</div>
 
 			<div className="input-set">
 				<label>
 					<span>Desconto (%)</span><br />
-					<input name="desconto" type="number" value={desconto} onChange={ (e) =>
-					setDesconto(Number (e.target.value))
-				} />
+					<input name="desconto" type="number" value={desconto} onChange={handleChange}
+					/>
 				</label>
 			</div>
 
 			<div className="result">
-				<div>Preço com desconto: </div>
-				<div>Classe: </div>
+				<div>Preço com desconto: R$ {precoDesconto.tofixed(2)} </div>
+				<div>Classe: {classDesconto()}</div>
 			</div>
 		</div>
 	</main>)
-}
 
 
 export default Prova1
